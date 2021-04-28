e953eb05e515d07703684c797d6cf1c25536e75b
shenlian0 <1005649614@qq.com>
2021-04-28 16:53:56 +0800

    创建项目

---
+++ erp-oauth2/src/main/java/com/erp/oauth2/config/OauthServerConfigurator.java
@@ -0,0 +1,62 @@
+package com.erp.oauth2.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+/**
+ * @author zwc
+ */
+public class OauthServerConfigurator extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JdbcUserDetailsService jdbcUserDetailsService;
+
+    /**
+     * 注册一个认证管理器对象到容器
+     */
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+
+    /**
+     * 密码编码对象（密码不进行加密处理）
+     *
+     * @return
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    /**
+     * 处理用户名和密码验证事宜
+     * 1）客户端传递username和password参数到认证服务器
+     * 2）一般来说，username和password会存储在数据库中的用户表中
+     * 3）根据用户表中数据，验证当前传递过来的用户信息的合法性
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // 在这个方法中就可以去关联数据库了，当前我们先把用户信息配置在内存中
+        // 实例化一个用户对象(相当于数据表中的一条用户记录)
+        /*UserDetails user = new User("admin","123456",new ArrayList<>());
+        auth.inMemoryAuthentication()
+                .withUser(user).passwordEncoder(passwordEncoder);*/
+
+        auth.userDetailsService(jdbcUserDetailsService).passwordEncoder(passwordEncoder);
+    }
+}
